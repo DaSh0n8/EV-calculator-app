6d744b2d80875dfaa017db364b6d5f5d6c2ddb32
Hayden Briese <haydenbriese@gmail.com>
2021-09-22 23:03:21 +1000

    Enable validation for public Calculator functions

--- app/calculator.py
+++ app/calculator.py
@@ -7,7 +7,8 @@ class Calculator:
 
     # you may add more parameters if needed, you may modify the formula also.
     def cost_calculation(self, initial_charge, final_charge, capacity, is_peak, is_holiday):
-        # validate(initial_charge=initial_charge, final_charge=final_charge, capacity=capacity)
+        validate(initial_charge=initial_charge, final_charge=final_charge, capacity=capacity)
+
         base_price = 100 if is_peak else 50
         surcharge_factor = 1.1 if is_holiday else 1
 
@@ -15,41 +16,42 @@ class Calculator:
 
     # you may add more parameters if needed, you may also modify the formula.
     def time_calculation(self, initial_charge, final_charge, capacity, power):
-        # validate(initial_charge=initial_charge, final_charge=final_charge, battery_capacity=capacity)
+        validate(initial_charge=initial_charge, final_charge=final_charge, capacity=capacity)
+
         return (final_charge - initial_charge) / 100 * capacity / power
 
     # you may create some new methods at your convenience, or modify these methods, or choose not to use them.
-    def is_holiday(self, start_date):
+    def __is_holiday(self, start_date):
         pass
 
-    def is_peak(self):
+    def __is_peak(self):
         pass
 
-    def peak_period(self, start_time):
+    def __peak_period(self, start_time):
         pass
 
-    def get_duration(self, start_time):
-        pass
-
-    # to be acquired through API
-    def get_sun_hour(self, sun_hour):
+    def __get_duration(self, start_time):
         pass
 
     # to be acquired through API
-    def get_solar_energy_duration(self, start_time):
+    def __get_sun_hour(self, sun_hour):
         pass
 
     # to be acquired through API
-    def get_day_light_length(self, start_time):
+    def __get_solar_energy_duration(self, start_time):
         pass
 
     # to be acquired through API
-    def get_solar_insolation(self, solar_insolation):
+    def __get_day_light_length(self, start_time):
         pass
 
     # to be acquired through API
-    def get_cloud_cover(self):
+    def __get_solar_insolation(self, solar_insolation):
         pass
 
-    def calculate_solar_energy(self):
+    # to be acquired through API
+    def __get_cloud_cover(self):
+        pass
+
+    def __calculate_solar_energy(self):
         pass
