dbc7af64a5e1dac765e536894396fc851aa90a15
Hayden Briese <haydenbriese@gmail.com>
2021-09-26 23:06:15 +1000

    Split start and end time into periods of an hour instead - required for REQ3

--- app/period.py
+++ app/period.py
@@ -1,15 +1,16 @@
 from datetime import date, time, datetime, timedelta
 
+
+def to_datetime(d: date, t: time) -> datetime:
+    return datetime(d.year, d.month, d.day, t.hour, t.minute, t.second)
+
+
 DAY_START = time(0)
 DAY_END = time(23, 59, 59)
 PEAK_START = time(6)
 PEAK_END = time(18)
-
-
-def minus_time(lhs: time, rhs: time) -> timedelta:
-    d = datetime.now()
-    return datetime(d.year, d.month, d.day, lhs.hour, lhs.minute, lhs.second) - \
-           datetime(d.year, d.month, d.day, rhs.hour, rhs.minute, rhs.second)
+BEFORE_PEAK = (to_datetime(date.today(), PEAK_START) - timedelta(seconds=1)).time()
+AFTER_PEAK = (to_datetime(date.today(), PEAK_END) + timedelta(seconds=1)).time()
 
 
 class Period:
@@ -18,6 +19,7 @@ class Period:
         self.start = start
         self.end = end
         assert start < end
+        assert start.hour == end.hour
 
     @property
     def base_price_factor(self) -> int:
@@ -51,59 +53,16 @@ class Period:
         return False
 
 
-def to_datetime(d: date, t: time) -> datetime:
-    return datetime(d.year, d.month, d.day, t.hour, t.minute, t.second)
-
-
-def split_by_day(start: datetime, end: datetime) -> [Period]:
-    assert start <= end
-    if start.date() == end.date():
-        return [Period(start.date(), start.time(), end.time())]
-
-    end_of_a = to_datetime(start.date(), DAY_END)
-    return [Period(start.date(), start.time(), end_of_a.time())] + split_by_day(end_of_a + timedelta(seconds=1), end)
-
-
-BEFORE_PEAK = (to_datetime(date.today(), PEAK_START) - timedelta(seconds=1)).time()
-AFTER_PEAK = (to_datetime(date.today(), PEAK_END) + timedelta(seconds=1)).time()
-
-
-def split_into_distinct_peak_periods(period: Period) -> [Period]:
-    """Splits the (start, end) period such into distinct non-overlapping peak and non-peak periods"""
-    start = period.start
-    end = period.end
-
-    def P(s: time, e: time) -> Period:
-        return Period(period.day, s, e)
-
-    # Case 1: period is outside of peak timing
-    if end < PEAK_START or PEAK_END < start:
-        return [P(start, end)]
-
-    # Case 2: period starts prior to peak
-    if start < PEAK_START:
-        pre = P(start, BEFORE_PEAK)
-        # Case 2.1: ends during peak
-        if end < PEAK_END:
-            return [pre, P(PEAK_START, end)]
-        # Case 2.2: ends after peak
-        else:
-            return [pre, P(PEAK_START, PEAK_END), P(AFTER_PEAK, end)]
-    # Case 3: period starts during peak
-    else:
-        # Case 3.1: ends during peak
-        if end < PEAK_END:
-            return [P(start, end)]
-        # Case 3.2: ends after peak
-        else:
-            return [P(start, PEAK_END), P(AFTER_PEAK, end)]
+def minus_time(lhs: time, rhs: time) -> timedelta:
+    d = datetime.now()
+    return to_datetime(d, lhs) - to_datetime(d, rhs)
 
 
 def split(start: datetime, end: datetime) -> [Period]:
-    day_periods: [Period] = split_by_day(start, end)
+    # Split into hour periods, to make estimating hourly solar generation (with cloud cover) easy
+    if start.date() == end.date() and start.hour == end.hour:
+        return [Period(start.date(), start.time(), end.time())]
 
-    distinct_periods = []
-    for p in day_periods:
-        distinct_periods += split_into_distinct_peak_periods(p)
-
-    return distinct_periods
+    end_of_hour = time(start.hour, 59, 59)
+    next_hour = to_datetime(start.date(), end_of_hour) + timedelta(seconds=1)
+    return [Period(start.date(), start.time(), end_of_hour)] + split(next_hour, end)
