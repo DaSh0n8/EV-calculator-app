794512816cf21c19cad6dc1f8506630801e25ab2
Hayden Briese <haydenbriese@gmail.com>
2021-10-02 00:34:57 +1000

    Renamed functions and made more static to ease testing

--- app/calculator.py
+++ app/calculator.py
@@ -4,15 +4,19 @@ from .period import *
 
 PANEL_SIZE = 50  # 50 m^2
 PANEL_EFFICIENCY = 0.2  # 20%
-AVERAGE_POINTS = 3
+
+TOTAL_DURATION_ZERO = ValidationError("Total duration must be greater than 0 seconds")
 
 
 class Calculator:
-    def __init__(self, api: WeatherApiInterface = WeatherApi()):
-        self.api = api
+    def __init__(self, api: WeatherApiInterface):
+        self.api: WeatherApiInterface = api
 
-    def cost(self, initial_charge: int, final_charge: int, capacity: int,
-             charger_config: str, start_date: date, start_time: time, postcode: int) -> float:
+    def total_cost(self, initial_charge: int, final_charge: int, capacity: int,
+                   charger_config: str, start_date: date, start_time: time, postcode: int) -> float:
+        validate(initial_charge=initial_charge, final_charge=final_charge, capacity=capacity,
+                 charger_config=charger_config, start_date=start_date, start_time=start_time)
+
         (power, price) = CHARGER_CONFIGS[charger_config]
         total_dur: timedelta = Calculator.charging_duration(initial_charge, final_charge, capacity, power)
 
@@ -21,41 +25,32 @@ class Calculator:
 
         total_cost = 0
         for p in periods:
-            charge = Calculator.period_charge(p, initial_charge, final_charge, total_dur)
-            total_cost += self.period_cost(p, charge, postcode, capacity, price)
+            charge_kwh = Calculator.charge_amount(p, initial_charge, final_charge, total_dur)
+            solar_generated = self.solar_generated(Period(p.day, p.start, p.end), postcode)
+            total_cost += self.period_cost(p, charge_kwh, price, solar_generated)
 
         return total_cost
 
-    def cost_calculation(self, initial_state, final_state, capacity, is_peak, is_holiday):
-        if is_peak:
-            base_price = 100
-        else:
-            base_price = 50
-
-        if is_holiday:
-            surcharge_factor = 1.1
-        else:
-            surcharge_factor = 1
-
-        cost = (final_state - initial_state) / 100 * capacity * base_price / 100 * surcharge_factor
-        return cost
+    @staticmethod
+    def period_cost(period: Period, charge_kwh: float, price: float, solar_generated: float) -> float:
+        cost = (charge_kwh - solar_generated) * price * period.base_price_factor * period.surcharge_factor
+        return max(cost, 0)
 
     @staticmethod
     def charging_duration(initial_charge: int, final_charge: int, capacity: int, power: float) -> timedelta:
+        validate(initial_charge=initial_charge, final_charge=final_charge, capacity=capacity)
         hours = (final_charge - initial_charge) / 100 * capacity / power
         return timedelta(hours=hours)
 
     @staticmethod
-    def period_charge(p: Period, initial_charge: int, final_charge: int, total_dur: timedelta) -> float:
+    def charge_amount(p: Period, initial_charge: int, final_charge: int, total_dur: timedelta) -> float:
+        """Returns: the amount the vehicle charged in the given period in kWh"""
+        validate(initial_charge=initial_charge, final_charge=final_charge)
+        if total_dur.total_seconds() == 0:
+            raise TOTAL_DURATION_ZERO
+
         charge_proportion = minus_time(p.end, p.start) / total_dur
         return (final_charge - initial_charge) * charge_proportion
-
-    def period_cost(self, period: Period, charge: float, postcode: int, capacity: int, price: float) -> float:
-        energy_used = charge / 100 * capacity
-        energy_charged = energy_used - self.solar_generated(Period(period.day, period.start, period.end), postcode)
-
-        cost = energy_charged * price * period.base_price_factor / 100 * period.surcharge_factor
-        return max(cost, 0)
 
     def solar_generated(self, period: Period, postcode: int) -> float:
         sunrise = self.api.sunrise(postcode, period.day)
@@ -74,18 +69,16 @@ class Calculator:
         return total
 
     @staticmethod
-    def __get_closest_avg_dates(day: date) -> [date]:
+    def get_past_years(day: date, past_years: int = 3) -> [date]:
         start_year = day.year if day < date.today() else date.today().year
-        return [date(start_year - i, day.month, day.day) for i in range(AVERAGE_POINTS)]
+        return [date(start_year - i, day.month, day.day) for i in range(past_years)]
 
     def __avg_solar_insolation(self, period: Period, postcode: int) -> float:
-        dates = Calculator.__get_closest_avg_dates(period.day)
+        dates = Calculator.get_past_years(period.day)
         values = [self.api.solar_insolation(postcode, d) for d in dates]
         return sum(values) / len(values)
 
     def __avg_cloud_cover(self, period: Period, postcode: int) -> float:
-        dates = Calculator.__get_closest_avg_dates(period.day)
+        dates = Calculator.get_past_years(period.day)
         values = [self.api.cloud_cover(postcode, d, period.start.hour) for d in dates]
         return sum(values) / len(values)
-
-
