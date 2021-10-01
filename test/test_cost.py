01deb76ab38103d292a97148891edb861ae4f11e
Brandon <blim0014@student.monash.edu>
2021-10-02 02:43:10 +1000

    adding test cases for TotalCostTests

--- test/test_cost.py
+++ test/test_cost.py
@@ -39,9 +39,66 @@ class PeriodCostTests(TestsBase):
 
 class TotalCostTests(TestsBase):
     def test_config_1_off_peak_without_surcharge_and_without_solar_generation(self):
-        # Off peak without surcharge
+        # Off peak without surcharge without solar generation
         day, start_time = date(2021, 9, 4), time(19)
         calc = mock_calc(si=0, sunrise=time(7), sunset=time(15), cc=0)
         total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
                                      start_date=day, start_time=start_time, postcode=3000)
         self.assertAlmostEqual(total_cost, 2.5, 2)
+
+    def test_config_1_off_peak_with_surcharge_and_without_solar_generation(self):
+        # Off peak with surcharge without solar generation
+        day, start_time = date(2021, 9, 6), time(19)
+        calc = mock_calc(si=0, sunrise=time(7), sunset=time(15), cc=0)
+        total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
+                                     start_date=day, start_time=start_time, postcode=3000)
+        self.assertAlmostEqual(total_cost, 2.7, 1)
+
+    def test_config_1_on_peak_without_surcharge_and_without_solar_generation(self):
+        # On peak without surcharge without solar generation
+        day, start_time = date(2021, 9, 4), time(13)
+        calc = mock_calc(si=0, sunrise=time(7), sunset=time(15), cc=0)
+        total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
+                                     start_date=day, start_time=start_time, postcode=3000)
+        self.assertAlmostEqual(total_cost, 5, 2)
+
+    def test_config_1_on_peak_with_surcharge_and_without_solar_generation(self):
+        # Off peak without surcharge without solar generation
+        day, start_time = date(2021, 9, 6), time(13)
+        calc = mock_calc(si=0, sunrise=time(7), sunset=time(15), cc=0)
+        total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
+                                     start_date=day, start_time=start_time, postcode=3000)
+        self.assertAlmostEqual(total_cost, 5.4, 0)
+
+    def test_config_1_off_peak_without_surcharge_and_with_solar_generation(self):
+        # Off peak without surcharge with solar generation
+        day, start_time = date(2021, 9, 4), time(19)
+        calc = mock_calc(si=5.6, sunrise=time(7), sunset=time(15), cc=0)
+        total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
+                                     start_date=day, start_time=start_time, postcode=3000)
+        self.assertAlmostEqual(total_cost, 9, 0)
+
+    def test_config_1_off_peak_with_surcharge_and_with_solar_generation(self):
+        # Off peak with surcharge with solar generation
+        day, start_time = date(2021, 9, 6), time(19)
+        calc = mock_calc(si=5.6, sunrise=time(7), sunset=time(15), cc=0)
+        total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
+                                     start_date=day, start_time=start_time, postcode=3000)
+        self.assertAlmostEqual(total_cost, 9.8, 0)
+
+    def test_config_1_on_peak_without_surcharge_and_with_solar_generation(self):
+        # On peak without surcharge with solar generation
+        day, start_time = date(2021, 9, 4), time(13)
+        calc = mock_calc(si=5.6, sunrise=time(7), sunset=time(15), cc=0)
+        total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
+                                     start_date=day, start_time=start_time, postcode=3000)
+        self.assertAlmostEqual(total_cost, 6.3, 0)
+
+    def test_config_1_on_peak_with_surcharge_and_with_solar_generation(self):
+        # Off peak without surcharge with solar generation
+        day, start_time = date(2021, 9, 6), time(13)
+        calc = mock_calc(si=5.6, sunrise=time(7), sunset=time(15), cc=0)
+        total_cost = calc.total_cost(initial_charge=0, final_charge=100, capacity=10, charger_config="1",
+                                     start_date=day, start_time=start_time, postcode=3000)
+        self.assertAlmostEqual(total_cost, 7, 0)
+
