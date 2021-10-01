c984bf5a0910c6e12b0789fc7ede140f9eb589f0
Brandon <blim0014@student.monash.edu>
2021-10-02 05:42:43 +1000

    deleting useless comments for test_validation.py

--- test/test_validation.py
+++ test/test_validation.py
@@ -13,7 +13,6 @@ class ValidationTests(TestsBase):
         self.assertValidation(Capacity.NotPositiveInteger, Capacity.validate, "abc")
 
     def test_soc(self):
-        # INITIAL AND FINAL SOC
         # Off-point value for initial SoC with on-point value for final SoC
         self.assertValidation(InitialCharge.NotPercentage, lambda: InitialCharge.validate(-1, 100))
         # Off-point value for initial SoC with invalid final SoC
@@ -28,24 +27,14 @@ class ValidationTests(TestsBase):
         self.assertValidation(InitialCharge.NotPercentage, lambda: InitialCharge.validate(-30, -50))
         # Off-point value for initial SoC and off-point value for final SoC
         self.assertValidation(InitialCharge.NotPercentage, lambda: InitialCharge.validate(-1, 101))
-        # On-point value for initial SoC and on-point value for final SoC
-        # self.assertEqual(self.soc_test(0, 100), 82.5)
         # Invalid initial SoC with off-point final SoC
         self.assertValidation(InitialCharge.NotPercentage, lambda: InitialCharge.validate(120, 100))
 
     def test_charger_config(self):
-        # CHARGER CONFIGURATION
-        # Description: Using boundary testing for charger configuration
         # Using an off-point value at the beginning of the range
         self.assertValidation(ChargerConfig.Invalid, lambda: ChargerConfig.validate("0"))
-        # Using an on-point value at the beginning of the range
-
-        # Using an on-point value at the end of the range
-
         # Using an off-point value at the end of the range
         self.assertValidation(ChargerConfig.Invalid, lambda: ChargerConfig.validate("9"))
-        # Using an in-point value
-
         # Using an out-point value
         self.assertValidation(ChargerConfig.Invalid, lambda: ChargerConfig.validate("12"))
         # Using an invalid input
