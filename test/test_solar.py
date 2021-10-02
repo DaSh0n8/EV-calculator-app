1c7f9d0e006e00f31d744f09debdcd4c7cd8266f
Brandon <blim0014@student.monash.edu>
2021-10-02 23:40:28 +1000

    updates for test requirements

--- test/test_solar.py
+++ test/test_solar.py
@@ -1,31 +1,83 @@
 from .testsbase import *
 
+past_date = date(2008, 7, 1)
+future_date = date(2024, 7, 1)
 
 class SolarTests(TestsBase):
-    def test_solar_generated_without_cloud_cover(self):
+    # solar generated test for past dates
+    def test_solar_generated_without_cloud_cover_req2(self):
         si, sunrise, sunset = 5.6, time(8), time(8, 59, 59)
         c = mock_calc(si=si, sunrise=sunrise, sunset=sunset)
-        r = c.solar_generated(Period(date.today(), sunrise, sunset), 1000)
+        r = c.solar_generated(Period(past_date, sunrise, sunset), 1000)
 
         # Almost equal is required due to floating point operations
         self.assertAlmostEqual(r, si * PANEL_SIZE * PANEL_EFFICIENCY, 2)
 
-    def test_partial_solar_generated_without_cloud_cover(self):
+    def test_partial_solar_generated_without_cloud_cover_req2(self):
+        si, sunrise, sunset = 5.6, time(8), time(10)
+        c = mock_calc(si=si, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(past_date, time(8), time(8, 59, 59)), 1000)
+        self.assertAlmostEqual(r, si * PANEL_SIZE * PANEL_EFFICIENCY / 2, 1)
+
+    def test_solar_generated_with_cloud_cover_req2(self):
+        si, cc, sunrise, sunset = 5.6, 0.5, time(8), time(8, 59, 59)
+        c = mock_calc(si=si, cc=cc, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(past_date, sunrise, sunset), 1000)
+        self.assertAlmostEqual(r, si * (1 - cc) * PANEL_SIZE * PANEL_EFFICIENCY, 2)
+
+    def test_partial_solar_generated_with_cloud_cover_req2(self):
+        si, cc, sunrise, sunset = 5.6, 0.5, time(8), time(10)
+        c = mock_calc(si=si, cc=cc, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(past_date, time(8), time(8, 59, 59)), 1000)
+        self.assertAlmostEqual(r, si * (1 - cc) * PANEL_SIZE * PANEL_EFFICIENCY / 2, 1)
+
+    def test_solar_generated_without_cloud_cover_current_date(self):
+        si, sunrise, sunset = 5.6, time(8), time(8, 59, 59)
+        c = mock_calc(si=si, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(date.today(), sunrise, sunset), 1000)
+        self.assertAlmostEqual(r, si * PANEL_SIZE * PANEL_EFFICIENCY, 2)
+
+    def test_partial_solar_generated_without_cloud_cover_current_date(self):
         si, sunrise, sunset = 5.6, time(8), time(10)
         c = mock_calc(si=si, sunrise=sunrise, sunset=sunset)
         r = c.solar_generated(Period(date.today(), time(8), time(8, 59, 59)), 1000)
         self.assertAlmostEqual(r, si * PANEL_SIZE * PANEL_EFFICIENCY / 2, 1)
 
-    def test_solar_generated_with_cloud_cover(self):
+    def test_solar_generated_with_cloud_cover_current_date(self):
         si, cc, sunrise, sunset = 5.6, 0.5, time(8), time(8, 59, 59)
         c = mock_calc(si=si, cc=cc, sunrise=sunrise, sunset=sunset)
         r = c.solar_generated(Period(date.today(), sunrise, sunset), 1000)
         self.assertAlmostEqual(r, si * (1 - cc) * PANEL_SIZE * PANEL_EFFICIENCY, 2)
 
-    def test_partial_solar_generated_with_cloud_cover(self):
+    def test_partial_solar_generated_with_cloud_cover_current_date(self):
         si, cc, sunrise, sunset = 5.6, 0.5, time(8), time(10)
         c = mock_calc(si=si, cc=cc, sunrise=sunrise, sunset=sunset)
         r = c.solar_generated(Period(date.today(), time(8), time(8, 59, 59)), 1000)
+        self.assertAlmostEqual(r, si * (1 - cc) * PANEL_SIZE * PANEL_EFFICIENCY / 2, 1)
+
+    # solar generated test for future dates
+    def test_solar_generated_without_cloud_cover_req3(self):
+        si, sunrise, sunset = 5.6, time(8), time(8, 59, 59)
+        c = mock_calc(si=si, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(future_date, sunrise, sunset), 1000)
+        self.assertAlmostEqual(r, si * PANEL_SIZE * PANEL_EFFICIENCY, 2)
+
+    def test_partial_solar_generated_without_cloud_cover_req3(self):
+        si, sunrise, sunset = 5.6, time(8), time(10)
+        c = mock_calc(si=si, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(future_date, time(8), time(8, 59, 59)), 1000)
+        self.assertAlmostEqual(r, si * PANEL_SIZE * PANEL_EFFICIENCY / 2, 1)
+
+    def test_solar_generated_with_cloud_cover_req3(self):
+        si, cc, sunrise, sunset = 5.6, 0.5, time(8), time(8, 59, 59)
+        c = mock_calc(si=si, cc=cc, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(future_date, sunrise, sunset), 1000)
+        self.assertAlmostEqual(r, si * (1 - cc) * PANEL_SIZE * PANEL_EFFICIENCY, 2)
+
+    def test_partial_solar_generated_with_cloud_cover_req3(self):
+        si, cc, sunrise, sunset = 5.6, 0.5, time(8), time(10)
+        c = mock_calc(si=si, cc=cc, sunrise=sunrise, sunset=sunset)
+        r = c.solar_generated(Period(future_date, time(8), time(8, 59, 59)), 1000)
         self.assertAlmostEqual(r, si * (1 - cc) * PANEL_SIZE * PANEL_EFFICIENCY / 2, 1)
 
     def test_past_years_from_past_date(self):
